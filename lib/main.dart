4b6af6b96b23093feab615d242feae30173fb22f
ruben12022 <57164046+ruben12022@users.noreply.github.com>
2019-11-06 15:01:25 -0700

    Add files via upload

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
-import 'package:bmi_calculator/input_page.dart';
 import 'package:flutter/material.dart';
+import 'input_page.dart';
 
 void main() => runApp(BMICalculator());
 
@@ -7,10 +7,10 @@ class BMICalculator extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
       theme: ThemeData.dark().copyWith(
-        primaryColor: Color(0xff0a0e21),
-        scaffoldBackgroundColor: Color(0xff0a0e21),
+        primaryColor: Color(0xFF0A0E21),
+        scaffoldBackgroundColor: Color(0xFF0A0E21),
+        //textTheme: TextTheme(body1: TextStyle(color: Colors.white)),
       ),
       home: InputPage(),
     );
